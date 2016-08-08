97ac924b57570312fee6dde8ff6c65080d4f3603
Honghao Zhang <zhh358@gmail.com>
2016-08-08 05:06:30 +0000

    update hello text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "Hello, Rio 2016 Olympic Games. Hello yyy."
     # https://www.rio2016.com/en/medal-count-country
   end
 

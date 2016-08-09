b75faf1b2f9f681e1cafab0152e979fbe7fcd0aa
Honghao Zhang <zhh358@gmail.com>
2016-08-09 03:54:41 +0000

    update home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello, Rio 2016 Olympic Games."
+    render text:
+    '
+      <h3>Rio 2016 Olympic Games - Medals API</h3>
+      <a href="/api/medal_by_country">/api/medal_by_country</a>
+    '
   end
 
 end
